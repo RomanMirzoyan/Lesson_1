36aaa825d9e21b906abd8ecde3cb80e7b80d056d
RomanMirzoyan <romaxa995@gmail.com>
2024-08-14 19:45:48 +0300

    Обработка возможных исключений

---
+++ SumArrayElements.java
@@ -0,0 +1,19 @@
+public class SumArrayElements {
+    
+    public static int sumArrayElements(String[][] array) {
+        if (array.length != 4 || array[0].length != 4) {
+            throw new MyArraySizeException("Размер массива должен быть 4x4");
+        }
+        int sum = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Некорректные данные в ячейке [" + i + "][" + j + "]");
+                }
+            }
+        }
+        return sum;
+    }
+}
